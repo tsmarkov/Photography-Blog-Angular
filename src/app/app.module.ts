8db3a69b1fb33c3619255c65304c5f4ad0bc4f7b
Цветомир Марков <markovcvetomir@gmail.com>
2018-08-10 20:13:38 +0300

    project setting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
+
+import { APP_KEY, APP_SECRET } from '../environments/evnvironment.variables'
+
+console.log(APP_KEY);
+console.log(APP_SECRET);
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/evnvironment.variables.ts
@@ -0,0 +1,2 @@
+export const APP_KEY =  "ap key";
+export const APP_SECRET = "ap sec";
